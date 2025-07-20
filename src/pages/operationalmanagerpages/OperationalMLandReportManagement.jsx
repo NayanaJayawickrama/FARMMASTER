9583aaa810807fa4ef67259c64f80286bc32d82f
NayanaJayawickrama <nayanajayawickrama12@gmail.com>
2025-07-20 13:23:46 +0530

    operational managers pages adding

---
+++ src/pages/operationalmanagerpages/OperationalMLandReportManagement.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Sidebar from "../../components/operationalmanagerdashboard/Sidebar";
+import LandReportManagement from "../../components/operationalmanagerdashboard/LandReportManagement";
+
+function OperationalMLandReportManagement() {
+  return (
+    <div className="flex min-h-screen">
+      <Sidebar />
+      <main className="flex-1 md:ml-64 p-4 min-h-screen">
+        <LandReportManagement />
+      </main>
+    </div>
+  );
+}
+
+export default OperationalMLandReportManagement;
